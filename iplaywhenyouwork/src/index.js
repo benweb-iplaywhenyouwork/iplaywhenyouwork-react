f4e249b178659734947e65a0e0a98f49d3f7a511
yeppin <yebins49@gmail.com>
2022-06-22 15:30:33 +0900

    feat: fix firebase json

--- iplaywhenyouwork/src/index.js
+++ iplaywhenyouwork/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const cors = require('cors')({
+  origin: true
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
